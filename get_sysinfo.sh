7dde5de09a3d15bfdcb8376762a10cc32c3c293f
xiangxiaoc <xiangxiaoc@vip.qq.com>
2019-11-05 04:20:32 +0800

    update

--- get_sysinfo.sh
+++ get_sysinfo.sh
@@ -1,43 +1,53 @@
 #!/bin/bash
 
-. /etc/os-release
-os_release_id=$ID$VERSION_ID
-
-
-# 分析系统基础信息
-
-clear
+[ -f /etc/os-release ] && . /etc/os-release
  
-# 提取操作系统信息
- 
-os=$(uname -o)
-distribution_name=$(. /etc/os-release;echo $ID)
-distribution_version=$(. /etc/os-release;echo $ID$VERSION_ID)
-cpu_architecture=$(uname -p)
-kernel_version=$(uname -r)
-hostname=$(hostname)
+printGreen='\e[1;32m'
+printGreen1='\e[1;32m'
+resetColor='\e[0m'
 
-# 提取网络信息
-ip=$(hostname -I | awk '{print $1}')
-public_ip=$(curl -s http://ipecho.net/plain)
-default_gateway_ip=$(netstat -rn | awk '{if($1=="0.0.0.0") print $2}')
-nameserver=$(cat /etc/resolv.conf | grep -E "\<nameserver[ ]+" | awk '{print $NF}')
+function getOSInfo() {
+    os=$( uname -o )
+    distribution_name=${ID}
+    distribution_version=${ID}${VERSION_ID}
+    distribution_subversion=${PRETTY_NAME}
+    cpu_architecture=$( uname -p )
+    kernel_version=$( uname -r )
+    hostname=$( hostname )
+}
 
-# 提取内存信息
-total_used_memory=$(awk '/MemTotal/{total=$2}/MemFree/{free=$2}END{print (total-free)/1024}' /proc/meminfo)M
-application_used_memory=$(awk '/MemTotal/{total=$2}/MemFree/{free=$2}/^Cached/{cached=$2}/Buffers/{buffers=$2}END{print (total-free-cached-buffers)/1024}' /proc/meminfo)M
-free_memory=$( awk '/MemFree/{print $2/1024}' /proc/meminfo )M
+function getNetworkInfo() {
+    ip=$( hostname -I | awk '{print $1}' )
+    public_ip=$(curl -s http://ipecho.net/plain)
+    default_gateway_ip=$( netstat -rn | awk '{if( $1 == "0.0.0.0" ) print $2}' )
+    nameserver=$(cat /etc/resolv.conf | grep -E "\<nameserver[ ]+" | awk '{print $NF}')
+}
 
-echo -e "\e[1;32m### basic info ### \e[0m"
+
+function getMemoryInfo() {
+    total_used_memory=$( awk '/MemTotal/{total=$2}/MemFree/{free=$2}END{print (total-free)/1024}' /proc/meminfo )M
+    application_used_memory=$( awk '/MemTotal/{total=$2}/MemFree/{free=$2}/^Cached/{cached=$2}/Buffers/{buffers=$2}END{print (total-free-cached-buffers)/1024}' /proc/meminfo )M
+    free_memory=$( awk '/MemFree/{print $2/1024}' /proc/meminfo )M
+}
+
+function getRunningStatus() {
+    now_time=$( date )
+    utc_time=$( date -u )
+    up_time=$( cat /proc/uptime| awk -F. '{run_days=$1 / 86400;run_hour=($1 % 86400)/3600;run_minute=($1 % 3600)/60;run_second=$1 % 60;printf("%dd %dh %dm %ds",run_days,run_hour,run_minute,run_second)}' )
+}
+
+getOSInfo
+echo -e "${printGreen1}### basic info ###${resetColor}"
 cat << EOF
-Hostname: ${hostname}
-OS Type: ${os}
+Hostname:   ${hostname}
+OS Type:    ${os}
 Distribution Version: $distribution_version
 Kernel Version: $kernel_version
 CPU Architecture: $cpu_architecture
 
 EOF
 
+getNetworkInfo
 echo -e "\e[1;32m### network info ### \e[0m"
 cat << EOF
 LAN IP: $ip
@@ -48,6 +58,7 @@ $nameserver
 
 EOF
 
+getMemoryInfo
 echo -e "\e[1;32m### memory info ### \e[0m"
 cat << EOF
 Total Used: $total_used_memory
@@ -58,16 +69,18 @@ EOF
 # 打印磁盘信息
 echo
 echo -e "\e[1;32m### disk info ### \e[0m"
-df -hP | grep -v 'tmpfs' | awk '{if($4!=0)print}'
+df -Th | awk '{ if($2 != "tmpfs") print }'
 
 # 基本运行现状
-echo
+getRunningStatus
 echo -e "\e[1;32m### running info ### \e[0m"
-w
+cat << EOF
+Local Time: ${now_time}
+UTC Time:   ${utc_time}
+UP Time:    ${up_time}
+EOF
 
 # 当前用户有什么crontab定时任务
 echo
 echo -e "\e[1;32m### crontab tasks ### \e[0m"
 crontab -l | grep -vE '^#|^$'
-
-echo
