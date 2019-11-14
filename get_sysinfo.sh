b6b8cdb70a189ba185c7b14bf747ead07a22aafc
xiangxiaoc <xiangxiaoc@vip.qq.com>
2019-11-14 15:45:07 +0800

    添加备注

--- get_sysinfo.sh
+++ get_sysinfo.sh
@@ -20,6 +20,7 @@ function getOSInfo() {
 function getNetworkInfo() {
     ip=$( hostname -I | awk '{print $1}' )
     public_ip=$(curl -s http://ipecho.net/plain)
+    # centos of mininal installation need install net-tools
     default_gateway_ip=$( netstat -rn | awk '{if( $1 == "0.0.0.0" ) print $2}' )
     nameserver=$(cat /etc/resolv.conf | grep -E "\<nameserver[ ]+" | awk '{print $NF}')
 }
