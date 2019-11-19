30d5f59de3c077811b617537c48a8e474d4d4100
xiangxiaoc <xiangxiaoc@vip.qq.com>
2019-11-19 08:40:12 +0800

    ready to code

---
+++ get_sysinfo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""
+Overview System Information
+"""
+
+import socket
+
+
+class SystemInfoCollector():
+    """
+
+    """
+
+    def get_os_info():
+        socket.ge
+
+
+def get_os_info():
