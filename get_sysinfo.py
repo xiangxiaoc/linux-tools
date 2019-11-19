fcb7be7f3708906a4b71064cbe2db0894bf71f34
xiangxiaoc <xiangxiaoc@vip.qq.com>
2019-11-19 18:28:27 +0800

    save

--- get_sysinfo.py
+++ get_sysinfo.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python
 
+
 """
 Overview System Information
 """
 
-import socket
+import platform
 
 
-class SystemInfoCollector():
-    """
+class SystemInfoCollector:
+    def __init__(self):
+        self.hostname = platform.node()
+        self.python_version = platform.python_version()
+        self.python_version_primary = platform.python_version_tuple()[0]
+        self.python_version_second = platform.python_version_tuple()[1]
+        self.python_version_third = platform.python_version_tuple()[2]
+        self.system = platform.system()  # Windows | Linux
+        if self.system == "Windows":
+            self.windows_version = platform.release()
+            self.windows_revision = platform.version()
+        elif self.system == "Linux":
+            self.linux_version = ''
 
-    """
 
-    def get_os_info():
-        socket.ge
-
-
-def get_os_info():
+if __name__ == "__main__":
+    a = SystemInfoCollector()
+    print(a.__dict__)
