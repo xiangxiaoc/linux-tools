ee4199492d965b2addba0ce7c068ed7bb5c3fc80
xiangxiaoc <xiangxiaoc@vip.qq.com>
2019-12-27 23:33:19 +0800

    format output

--- find_subprocess.sh
+++ find_subprocess.sh
@@ -4,7 +4,7 @@ function cumulate_pids() {
     ## Recursive function for cumulate pid
     local process_id=$1
     local subprocess_pids=$(ps -ef | awk '{if($3=="'$process_id'"){print $2}}')
-    pids="$pids $subprocess_pids"
+    pids=$(echo $pids $subprocess_pids)
     for i in $subprocess_pids; do
         cumulate_pids $i
     done
