82e594d9585e5e39854c7ab5e55c79a261be9ad0
xiangxiaoc <xiangxiaoc@vip.qq.com>
2020-04-05 21:53:43 +0800

    add !@#$%^&*

--- random_string.py
+++ random_string.py
@@ -10,19 +10,24 @@ Generate random string
 
 import random
 import string
-import sys
+
+from argparse import ArgumentParser
 
 
 class RandomStringGenerator:
     suggest_str_max_len = 1024
 
-    def __init__(self, str_type='both', str_len=8):
-        self.name = str_type + '_str_generator'
-        self.__type = str_type
+    def __init__(self, str_len, sp):
+        self.name = 'run'
+        self.__type = 'both'
         self.__len = str_len
+        self.__sp = sp
 
     def generate_string(self):
-        if self.__type == 'both':
+        if self.__sp:
+            repeat_time = int(self.__len / 70 + 1)
+            return ''.join(random.sample((string.ascii_letters + string.digits + '!@#$%^&*') * repeat_time, self.__len))
+        elif self.__type == 'both':
             repeat_time = int(self.__len / 62 + 1)
             return ''.join(random.sample((string.ascii_letters + string.digits) * repeat_time, self.__len))
         elif self.__type == 'letter':
@@ -33,13 +38,18 @@ class RandomStringGenerator:
             return ''.join(random.sample(string.digits * repeat_time, self.__len))
 
 
+def parse_arguments():
+    parser = ArgumentParser(description="generate secret")
+    parser.add_argument("-l", "--length", required=False, type=int, metavar='num', default=8,
+                        help="length of secret")
+    parser.add_argument("-s", "--spec-char", action="store_true", default=False,
+                        help="secret with special characters")
+
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    try:
-        input_str_len = int(sys.argv[1])
-    except IndexError:
-        input_str_len = 8
-    try:
-        input_str_type = sys.argv[2]
-    except IndexError:
-        input_str_type = "both"
-    print(RandomStringGenerator(str_len=input_str_len, str_type=input_str_type).generate_string())
+    args = parse_arguments()
+    run = RandomStringGenerator(str_len=args.length, sp=args.spec_char)
+    secret = run.generate_string()
+    print(secret)
