6c96089988df85cb94c2e835178613e6a9715e87
xiangxiaoc <xiangxiaoc@vip.qq.com>
2020-04-20 00:08:43 +0800

    format

--- configure-vimrc.sh
+++ configure-vimrc.sh
@@ -11,33 +11,33 @@ author_name='$author_name'
 author_email='$author_email'
 
 function append_str() {
-cat << EOF 
+    cat <<EOF
 autocmd BufNewFile *.py,*.sh, exec ":call SetTitle()"
 let $author_name = "xiangxiaoc"
 let $author_email = "xiangxiaoc@vip.qq.com"
 
 func SetTitle()
 if &filetype == 'sh'
-        call setline(1,"\#!/bin/bash")
-        call append(line("."), "")
-        call append(line(".")+1, "\##################################################")
-        call append(line(".")+2, "\# File Name: ".expand("%"))
-        call append(line(".")+3, "\# Author: ".$author_name)
-        call append(line(".")+4, "\# Email: ".$author_email)
-        call append(line(".")+5, "\# Created Time: ".strftime("%c"))
-        call append(line(".")+6, "\##################################################")
-        call append(line(".")+7, "")
+    call setline(1,"\#!/bin/bash")
+    call append(line("."), "")
+    call append(line(".")+1, "\##################################################")
+    call append(line(".")+2, "\# File Name: ".expand("%"))
+    call append(line(".")+3, "\# Author: ".$author_name)
+    call append(line(".")+4, "\# Email: ".$author_email)
+    call append(line(".")+5, "\# Created Time: ".strftime("%c"))
+    call append(line(".")+6, "\##################################################")
+    call append(line(".")+7, "")
 else
-        call setline(1,"\#!/usr/bin/env python")
-        call append(line("."), "\#coding:uft-8")
-        call append(line(".")+1, "")
-        call append(line(".")+2, "\##################################################")
-        call append(line(".")+3, "\# File Name: ".expand("%"))
-        call append(line(".")+4, "\# Author: ".$author_name)
-        call append(line(".")+5, "\# Email: ".$author_email)
-        call append(line(".")+6, "\# Created Time: ".strftime("%c"))
-        call append(line(".")+7, "\##################################################")
-        call append(line(".")+8, "")
+    call setline(1,"\#!/usr/bin/env python")
+    call append(line("."), "\#coding:uft-8")
+    call append(line(".")+1, "")
+    call append(line(".")+2, "\##################################################")
+    call append(line(".")+3, "\# File Name: ".expand("%"))
+    call append(line(".")+4, "\# Author: ".$author_name)
+    call append(line(".")+5, "\# Email: ".$author_email)
+    call append(line(".")+6, "\# Created Time: ".strftime("%c"))
+    call append(line(".")+7, "\##################################################")
+    call append(line(".")+8, "")
 endif
 
 autocmd BufNewFile * normal G
@@ -66,4 +66,4 @@ set pastetoggle=<F9>
 EOF
 }
 
-append_str >> ~/.vimrc
+append_str >>~/.vimrc
