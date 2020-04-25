53956281963bbc28885255da103a8ea808761ef2
xiangxiaoc <xiangxiaoc@vip.qq.com>
2020-04-26 01:11:00 +0800

    recode

--- random_string.py
+++ random_string.py
@@ -8,34 +8,42 @@
 Generate random string
 """
 
-import random
-import string
-
+from string import ascii_letters, digits
+from random import sample
+from math import ceil
 from argparse import ArgumentParser
 
 
 class RandomStringGenerator:
     suggest_str_max_len = 1024
 
-    def __init__(self, str_len, sp):
-        self.name = 'run'
+    def __init__(self, str_len, special_char=False):
         self.__type = 'both'
         self.__len = str_len
-        self.__sp = sp
+        self.__sp = special_char
 
     def generate_string(self):
+        total_str_library = ''
+        char_pool_num = 0
         if self.__sp:
-            repeat_time = int(self.__len / 70 + 1)
-            return ''.join(random.sample((string.ascii_letters + string.digits + '!@#$%^&*') * repeat_time, self.__len))
-        elif self.__type == 'both':
-            repeat_time = int(self.__len / 62 + 1)
-            return ''.join(random.sample((string.ascii_letters + string.digits) * repeat_time, self.__len))
-        elif self.__type == 'letter':
-            repeat_time = int(self.__len / 52 + 1)
-            return ''.join(random.sample(string.ascii_letters * repeat_time, self.__len))
-        elif self.__type == 'digit':
-            repeat_time = int(self.__len / 10 + 1)
-            return ''.join(random.sample(string.digits * repeat_time, self.__len))
+            str_library = '!@#$%^&*'
+            total_str_library += str_library
+            char_pool_num += str_library.__len__()
+        if self.__type == 'both':
+            str_library = ascii_letters + digits
+            total_str_library += str_library
+            char_pool_num += str_library.__len__()
+        if self.__type == 'letter':
+            str_library = ascii_letters
+            total_str_library += str_library
+            char_pool_num += str_library.__len__()
+        if self.__type == 'digit':
+            str_library = digits
+            total_str_library += str_library
+            char_pool_num += str_library.__len__()
+        repeat_time = ceil(self.__len / char_pool_num)
+        char_list = sample(total_str_library * repeat_time, self.__len)
+        return ''.join(char_list)
 
 
 def parse_arguments():
@@ -50,6 +58,6 @@ def parse_arguments():
 
 if __name__ == '__main__':
     args = parse_arguments()
-    run = RandomStringGenerator(str_len=args.length, sp=args.special_char)
+    run = RandomStringGenerator(args.length, args.special_char)
     secret = run.generate_string()
     print(secret)
