266d5ea2437fac8e0d51fdad063e3e237cd767b1
xiangxiaoc <xiangxiaoc@vip.qq.com>
2020-07-10 14:48:44 +0800

    functionalize

--- check_mount.sh
+++ check_mount.sh
@@ -6,17 +6,28 @@ CR='\e[0;31m'
 CG='\e[0;32m'
 RC='\e[0m'
 
-if [ ! $UID -eq 0 ];then
-    echo "Only for root"
-    exit
-fi
-
-mount_point_list=$(grep -v "^#" /etc/fstab | awk '{print $2}')
-echo -e "Status      Mount Point"
-for mount_point in $mount_point_list; do
-    if df "$mount_point" &>/dev/null; then
-        echo -e "${CG}mounted${RC}      $mount_point"
-    else
-        echo -e "${CR}unmounted${RC}    $mount_point"
+function check_permission() {
+    if [ ! $UID -eq 0 ]; then
+        echo "Only for root"
+        exit
     fi
-done
+}
+
+function check_mount() {
+    mount_point_list=$(grep -v "^#" /etc/fstab | awk '{print $2}')
+    for mount_point in $mount_point_list; do
+        if df "$mount_point" &>/dev/null; then
+            echo -e "${CG}mounted${RC}      $mount_point"
+        else
+            echo -e "${CR}unmounted${RC}    $mount_point"
+        fi
+    done
+}
+
+function main_process() {
+    check_permission
+    echo -e "Status       Mount Point"
+    check_mount
+}
+
+main_process
