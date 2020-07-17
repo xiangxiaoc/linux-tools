dd0bf18e370a6fcac2fe27799e61dcb7fedfb5b7
xiangxiaoc <xiangxiaoc@vip.qq.com>
2020-07-17 10:57:43 +0800

    release

--- check_mount.sh
+++ check_mount.sh
@@ -2,9 +2,22 @@
 
 ## Check whether Mount points is mounted from /etc/fstab
 
+SCRIPT_VERSION='v1'
+SCRIPT_UPDATE_DATE=2020-07-17
+SCRIPT_UPDATE_TIME=10:45
+SCRIPT_UPDATE_TZ=UTC+8
+
 CR='\e[0;31m'
 CG='\e[0;32m'
+CY='\e[1;33m'
 RC='\e[0m'
+
+function get_script_version() {
+    cat <<EOF_show_version
+Version: $SCRIPT_VERSION
+Update:  $SCRIPT_UPDATE_DATE $SCRIPT_UPDATE_TIME $SCRIPT_UPDATE_TZ
+EOF_show_version
+}
 
 function check_permission() {
     if [ $UID -ne 0 ]; then
@@ -14,20 +27,40 @@ function check_permission() {
 }
 
 function check_mount() {
-    mount_point_list=$(grep -v "^#" /etc/fstab | awk '{print $2}')
-    for mount_point in $mount_point_list; do
-        if df "$mount_point" &>/dev/null; then
-            echo -e "${CG}mounted${RC}      $mount_point"
+    local list=$1
+    for path in $list; do
+        real_path=$(readlink -f "$path")
+        if [ -z "$real_path" ]; then
+            echo -e "${CR}dir non exist${RC}            $path"
+            continue
+        fi
+        if mount | grep "${real_path%/}" &>/dev/null; then
+            if eval ls "$real_path/*" &>/dev/null; then
+                echo -e "${CG}mounted${RC}                  $path"
+            else
+                echo -e "${CY}problematic(no file)${RC}     $path"
+            fi
         else
-            echo -e "${CR}unmounted${RC}    $mount_point"
+            if eval ls "$real_path"/* &>/dev/null; then
+                echo -e "${CG}mounted${RC}                  $path"
+            else
+                echo -e "${CR}unmounted${RC}                $path"
+            fi
         fi
     done
 }
 
 function main_process() {
-    check_permission
-    echo -e "Status       Mount Point"
-    check_mount
+    sub_command=$1
+    case $sub_command in
+    '')
+        check_permission
+        echo -e "Status                   Mount Point"
+        mount_point_list=$(grep -v "^#" /etc/fstab | awk '{print $2}')
+        check_mount "$mount_point_list"
+        ;;
+    version) get_script_version ;;
+    esac
 }
 
-main_process
+main_process "$@"
